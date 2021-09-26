316d409b1be204d63251510c94599acba52fa344
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-09-26 16:34:20 +0700

    add blob

--- algorithms/src/blob.rs
+++ algorithms/src/blob.rs
@@ -1,1 +1,18 @@
+use ndarray::prelude::*;
 
+pub fn distribute(server: i32, job: i32) -> Array2<i32> {
+    // TODO: Implement distribute array
+    return array![[0, 1], [2, 3]];
+}
+
+#[cfg(test)]
+mod blob_test {
+    use super::*;
+
+    #[test]
+    fn it_2_server_4_job_should_return_0_1_and_2_3() {
+        let actual = distribute(2, 4);
+        let expect = array![[0, 1], [2, 3]];
+        assert_eq!(actual, expect);
+    }
+}
