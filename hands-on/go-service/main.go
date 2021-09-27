75752079af212e92e6498deaed4437f3010ba36c
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-09-27 20:08:44 +0700

    deploy v.3 go

--- hands-on/go-service/main.go
+++ hands-on/go-service/main.go
@@ -12,24 +12,28 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
 func main() {
 	app := fiber.New()
-	env, port := "", ""
+	env, port := "dev", "3001"
 
 	err := godotenv.Load(".env")
 	if err != nil {
-		port = "3001"
-		env = "dev"
 		print("Error loading .env file")
-	} else {
-		port = os.Getenv("PORT")
-		env = os.Getenv("ENV")
 	}
 
-	fmt.Printf("%s, %s", port, env)
+	env = getenv("ENV", "dev")
+	port = getenv("PORT", "3001")
 
 	app.Get("/go", func(c *fiber.Ctx) error {
-		msg := fmt.Sprintf("Go Service on env : %s -- version 0.0.1 !", env)
+		msg := fmt.Sprintf("Go Service on env : %s -- version 0.0.3 !", env)
 		return c.JSON(Response{Message: msg})
 	})
 
