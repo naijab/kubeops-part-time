8dacedfaa7cd75cb63c00efc88427db7384046f3
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-09-27 18:29:40 +0700

    change to v 9

--- hands-on/node-service/src/index.js
+++ hands-on/node-service/src/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.get("/node", (req, res) => {
   return res.json({
-    message: `Node Service on env : ${env} -- version 0.0.6 !`,
+    message: `Node Service on env : ${env} -- version 0.0.9 !`,
   });
 });
 
